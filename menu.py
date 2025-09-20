874c5d542b63793104f9f53c922a6db9f08cd882
tarus <victortarus6@gmail.com>
2025-09-20 12:56:40 +0930

    assignments

---
+++ menu.py
@@ -0,0 +1,27 @@
+#This is my first python code
+#Tarvk002
+
+
+print("Welcome to the Maweson Lakes Deli");
+print("Hello!")
+name =  input("What is your name ? : ")
+print ('Hello ',name,'!')
+
+print();
+print("======= Drinks ======");
+print("Coke, Sprite , Lift , Orange Juice")
+drink = input("What woud you like to drink? ")
+print("Your choice of drink is:", drink)
+
+print()
+print("======= Main Dishes ======");
+print("Pancakes , Noodles, Meat Pie, Fish & Chips, Dumplings")
+main_dish = input("What woud you like to eat? ")
+print("Your choice of main dish is:", main_dish)
+
+
+print()
+print("======= Desserts ======");
+print("Ice cream , Cake, Brownies, Cookies")
+dessert = input("What woud you like for dessert ")
+print("Your choice of dessert is:", dessert)
