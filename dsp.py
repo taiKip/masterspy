e4f2f0c9331ffaf573a029a1b1b27cf60fc4e060
tarus <victortarus6@gmail.com>
2025-11-16 14:24:51 +1030

    dsa done

---
+++ dsp.py
@@ -0,0 +1,20 @@
+c = [-45,6,0,72,1543]
+
+for i in c:
+    print(i)
+
+
+print(f"the last item : {c[4]}")
+
+s = 'hello'
+print(len(s))
+
+
+a_list = []
+
+for number in range(1,6):
+    a_list.append(number)
+
+
+for j in a_list:
+    print(j)
